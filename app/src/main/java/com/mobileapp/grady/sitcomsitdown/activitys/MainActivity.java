016887c50afc789538ba1fbad13a68afea2c3179
GradyLealand <kineco@gmail.com>
2018-04-23 09:39:31 -0300

    finished adaptor

--- app/src/main/java/com/mobileapp/grady/sitcomsitdown/activitys/MainActivity.java
+++ app/src/main/java/com/mobileapp/grady/sitcomsitdown/activitys/MainActivity.java
@@ -2,14 +2,40 @@ package com.mobileapp.grady.sitcomsitdown.activitys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.mobileapp.grady.sitcomsitdown.R;
+import com.mobileapp.grady.sitcomsitdown.database.SitcomAdapter;
+import com.mobileapp.grady.sitcomsitdown.database.SitcomDBHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private SitcomDBHelper dbHelper;
+    private SitcomAdapter adapter;
+    private final int GRID_ROWS = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize variables
+        mRecyclerView = (RecyclerView)findViewById(R.id.rv);
+        mRecyclerView.setHasFixedSize(true);
+        //set a grid layout manager
+        mLayoutManager = new GridLayoutManager(this.getApplicationContext(), GRID_ROWS);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        //populate the recycler view
+        populaterecyclerView();
+    }
+
+    private void populaterecyclerView()
+    {
+        dbHelper = new SitcomDBHelper(this);
+        adapter = new SitcomAdapter(dbHelper.getSitcoms(), this, mRecyclerView);
+
     }
 }
---
+++ app/src/main/java/com/mobileapp/grady/sitcomsitdown/database/SitcomAdapter.java
@@ -0,0 +1,82 @@
+package com.mobileapp.grady.sitcomsitdown.database;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mobileapp.grady.sitcomsitdown.R;
+import com.mobileapp.grady.sitcomsitdown.activitys.MainActivity;
+import com.mobileapp.grady.sitcomsitdown.models.Sitcom;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class SitcomAdapter extends RecyclerView.Adapter<SitcomAdapter.ViewHolder>
+{
+    private List<Sitcom> mSitcomList;
+    private Context mContext;
+    private RecyclerView mRecyclerView;
+
+    public SitcomAdapter(List<Sitcom> sitcoms, Context context, RecyclerView mRecyclerView)
+    {
+        mSitcomList = sitcoms;
+        mContext = context;
+        this.mRecyclerView = mRecyclerView;
+    }
+
+    /**
+     * A view for each individual item
+     */
+    public class ViewHolder extends RecyclerView.ViewHolder
+    {
+        public TextView tvSitcomName;
+        public ImageView ivSitcomImage;
+
+        public View layout;
+
+        public ViewHolder(View v)
+        {
+            super(v);
+            layout = v;
+            tvSitcomName = v.findViewById(R.id.tv_sitcom_name);
+            ivSitcomImage = v.findViewById(R.id.iv_sitcom_image);
+        }
+    }
+
+    @NonNull
+    @Override
+    public SitcomAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        LayoutInflater inflater = LayoutInflater.from( parent.getContext());
+        View v = inflater.inflate(R.layout.title_card, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SitcomAdapter.ViewHolder holder, int position)
+    {
+        //set onClick listener
+        holder.layout.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSitcomList.size();
+    }
+}
